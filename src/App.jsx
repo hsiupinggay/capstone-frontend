a35ec1acf9ea00a9a3b132e1c0f35de6de055f3b
Nuuggs <bryluke000@gmail.com>
2022-03-05 18:57:18 +0800

    react router structure set

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+
 
 export default function App() {
   return (
-    <div >
-     
+    <div>
+     <Router>
+       <NavLink></NavLink>
+
+       <Routes>
+         <Route />
+       </Routes>
+     </Router>
     </div>
   );
 }
