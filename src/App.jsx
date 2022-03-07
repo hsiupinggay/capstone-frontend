ca4d73da8966b97659928008a6ccd0219fbb5abf
Nuuggs <bryluke000@gmail.com>
2022-03-07 08:08:31 +0800

    added navbar, routes, and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import axios from 'axios';
 import { MedicalProvider } from './components/others/store'
-// Components import
+
+// Pages import
 import AuthPage from './components/pages/AuthPage';
+import HomePage from './components/pages/HomePage';
+import AppointmentsPage from './components/pages/AppointmentsPage';
+import FamilyPage from './components/pages/FamilyPage';
+import ProfilePage from './components/pages/ProfilePage';
+import LogoutPage from './components/pages/LogoutPage';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || `http://localhost:3004`;
 
 export default function App() {
   // state to check jwt from backend
-  const [auth, setAuth] = useState(false);
+  const [auth, setAuth] = useState(true);
 
   // on load, check for token... if no token, 
   // useEffect(() => {
@@ -39,8 +45,21 @@ export default function App() {
       <Router>
         { auth  ?
           <div className="main-container w-screen h-screen flex flex-col items-center">
+            <div className="nav-div w-full border-solid border-black border-2">
+              <nav className="flex justify-evenly items-center">
+                <NavLink className={({ isActive }) => `${isActive && `font-bold`} hover:underline`} to="/">Home</NavLink>
+                <NavLink className={({ isActive }) => `${isActive && `font-bold`} hover:underline`} to="/appointments">Appointments</NavLink>
+                <NavLink className={({ isActive }) => `${isActive && `font-bold`} hover:underline`} to="/family">Family || People(?)</NavLink>
+                <NavLink className={({ isActive }) => `${isActive && `font-bold`} hover:underline`} to="/profile">Profile</NavLink>
+                <NavLink className={({ isActive }) => `${isActive && `font-bold`} hover:underline`} to="/logout">Logout</NavLink>
+              </nav>
+            </div>
             <Routes>
-              <Route />
+              <Route path="/" element={<HomePage auth={auth} />} />
+              <Route path="/appointments" element={<AppointmentsPage auth={auth} />} />
+              <Route path="/family" element={<FamilyPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/logout" element={<LogoutPage setAuth={setAuth} />} />
             </Routes>
           </div> 
           // Log In page displays if there's no auth token
---
+++ src/components/pages/FamilyPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FamilyPage = () => {
+  return (
+    <div>FamilyPage</div>
+  );
+};
+
+export default FamilyPage;
