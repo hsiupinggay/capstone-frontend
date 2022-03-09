17dc3a13bf45e6e07876dce0b46583951cb1a493
shannonssd <shannonssd@gmail.com>
2022-03-09 21:27:43 +0800

    Completed jsx for adding appointment

--- src/components/organisms/AddAppointment.jsx
+++ src/components/organisms/AddAppointment.jsx
@@ -8,11 +8,13 @@ export default function AddAppointment() {
   const [deptArr, setDeptArr] = useState([]);
   const [chaperoneArr, setChaperoneArr] = useState([]);
   const [patientId, setPatientId] = useState("");
+  const [patientName, setPatientName] = useState("");
   const [hospital, setHospital] = useState("");
   const [chaperone, setChaperone] = useState("");
   const [chaperoneId, setChaperoneId] = useState("");
   const [department, setDepartment] = useState("");
   const [dateTime, setDateTime] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
   // When component renders, retrieve all patient data related to user 
   useEffect(() => {
@@ -27,10 +29,13 @@ export default function AddAppointment() {
   }, []);
 
   // When user has selected a patient, find related hospitals and chaperones
-  const updateHosChapDropdowns = (id) => {
-    setPatientId(id);
+  const updateHosChapDropdowns = (string) => {
+    console.log('string', string);
+    const patientSplitStr = string.split(",");
+    setPatientId(patientSplitStr[0]);
+    setPatientName(patientSplitStr[1]);
     for (let i = 0; i < patientArr.length; i +=1){
-      if (patientArr[i]._id === id) {
+      if (patientArr[i]._id === patientSplitStr[0]) {
         setChaperoneArr(patientArr[i].visitDetails.chaperones);
         setHospArr(patientArr[i].visitDetails.clinics);
       }
@@ -66,7 +71,37 @@ export default function AddAppointment() {
       dateTime,
     }
     console.log('data', data);
-    axios.post(`${process.env.REACT_APP_BACKEND_URL}/patient/add-appointment`, data).then((response) => {console.log(response.data)});
+    axios.post(`${process.env.REACT_APP_BACKEND_URL}/patient/add-appointment`, data).then((response) => {
+      console.log(response.data)
+      if(response.status === 200) {
+        setSuccessMessage(
+        <div> 
+          <p>
+            {`You have added a new appointment: `}
+          </p>
+          <p>
+            {`Patient: ${patientName}`}
+          </p>
+          <p>
+            {`Hospital: ${hospital}`}
+          </p>
+          <p>
+            {`Department: ${department}`}
+          </p>
+          <p>
+            {`Date: ${response.data.data[response.data.data.length-1].date}`}
+          </p>
+          <p>
+            {`Time: ${response.data.data[response.data.data.length-1].time}`}
+          </p>
+          <p>
+            {`Chaperone: ${chaperone || "Nil"}`}
+          </p>
+        </div>  
+        ) 
+          
+      }
+    });
   };
 
   return (
@@ -76,70 +111,117 @@ export default function AddAppointment() {
       : (
       <form onSubmit={handleSubmit}>
 
-        <label for="dateTime" name="dateTime"></label>
-        <input type="datetime-local" id="dateTime" name="dateTime" onChange={(event) => setDateTime(event.target.value)}/>
+        <div>
+          <label for="dateTime" name="dateTime"></label>
+          <input type="datetime-local" id="dateTime" name="dateTime" onChange={(event) => setDateTime(event.target.value)} required/>
+        </div>
 
+        <div>
         <label htmlFor="patient"> </label>
-        <select name="patient" id="patient" onChange={(event) => updateHosChapDropdowns(event.target.value)} >
-          <option disabled selected>Select Patient</option>
+        <select name="patient" id="patient" onChange={(event) => updateHosChapDropdowns(event.target.value)} required>
+          <option value="" disabled selected>Select Patient</option>
           { 
             patientArr.map((patient, index) => ( 
-              <option value={patient._id} key={index}>
+              <option value={`${patient._id},${patient.identity.name.first + " " + patient.identity.name.last}`} key={index}>
                 {patient.identity.name.first + " " + patient.identity.name.last}
               </option>
             )) 
           }
         </select>
+        </div>
 
-        { hospArr.length === 0  
-        ? <div/> 
-        : <div>
-            <label htmlFor="hospital"> </label>
-            <select name="hospital" id="hospital" onChange={(event) => updateDept(event.target.value)}>
+        { 
+        hospArr.length === 0  
+        ? 
+        <div>
+          <label htmlFor="hospital"> </label>
+          <select >
             <option disabled selected>Select Hospital</option>
-            { 
-              hospArr.map((hospital, index) => ( 
-                <option value={hospital.hospital} key={index}>
-                  {hospital.hospital}
-                </option>
-              )) 
-            }
+          </select>
+           <label htmlFor="department"> </label>
+          <div>
+            <select >
+              <option disabled selected>Select Department</option>
             </select>
+          </div>
+          <label htmlFor="chaperone"> </label>
+          <div>
+            <select >
+              <option disabled selected>Select Chaperone</option>
+            </select>
+          </div>
+        </div>  
+        : 
+        <div>
+            <div>
+              <label htmlFor="hospital"> </label>
+              <select name="hospital" id="hospital" onChange={(event) => updateDept(event.target.value)} required>
+              <option value="" disabled selected>Select Hospital</option>
+              { 
+                hospArr.map((hospital, index) => ( 
+                  <option value={hospital.hospital} key={index}>
+                    {hospital.hospital}
+                  </option>
+                )) 
+              }
+              </select>
+            </div>
 
-            <label htmlFor="chaperone"> </label>
-            <select name="chaperone" id="chaperone" onChange={(event) => updateChaperoneState(event.target.value)}>
-            <option disabled selected>Select Chaperone</option>
-            { 
-              chaperoneArr.map((chaperone, index) => ( 
-                <option value={`${chaperone.name},${chaperone.chaperoneId}`} key={index}>
-                  {chaperone.name}
-                </option>
-              )) 
+            {
+            deptArr.length === 0  
+            ? 
+            <div>
+              <label htmlFor="department"> </label>
+              <select >
+                <option disabled selected>Select Department</option>
+              </select>
+            </div>  
+            : 
+            <div>
+                <label htmlFor="department"> </label>
+                <select name="department" id="department" onChange={(event) => setDepartment(event.target.value)} required>
+                <option value="" disabled selected>Select Department</option>
+                { 
+                  deptArr.map((department, index) => ( 
+                    <option value={department} key={index}>
+                      {department}
+                    </option>
+                  )) 
+                }
+                </select>
+              </div>
             }
-            </select>
+            
+            <div>
+              <label htmlFor="chaperone"> </label>
+              <select name="chaperone" id="chaperone" onChange={(event) => updateChaperoneState(event.target.value)}>
+              <option disabled selected>Select Chaperone</option>
+              { 
+                chaperoneArr.map((chaperone, index) => ( 
+                  <option value={`${chaperone.name},${chaperone.chaperoneId}`} key={index}>
+                    {chaperone.name}
+                  </option>
+                )) 
+              }
+              </select>
+            </div>
           </div>
         }
 
-        {
-        deptArr.length === 0  
-        ? <div/> 
-        : <div>
-            <label htmlFor="department"> </label>
-            <select name="department" id="department" onChange={(event) => setDepartment(event.target.value)}>
-            <option disabled selected>Select Department</option>
-            { 
-              deptArr.map((department, index) => ( 
-                <option value={department} key={index}>
-                  {department}
-                </option>
-              )) 
-            }
-            </select>
-          </div>
-        }
         <button type="submit"> Submit</button>
       </form> 
       )}
+      <div>
+        {
+        successMessage === ""
+        ? 
+        <div/>
+        : 
+        <div>
+          {successMessage}
+        </div>
+        }
+      </div>
     </div>
   )
 }
