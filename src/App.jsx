49cd1e5222e48437a9eb5125ed43de9cc20f60f7
Hsiu Ping Gay <hsiups@192.168.1.5>
2022-03-10 19:57:10 +0700

    added protected routes + nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
-  BrowserRouter as Router, Routes, Route, NavLink,
+  BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
-import axios from 'axios';
 import { MedicalProvider } from './components/others/store';
+import ProtectedRoute from './components/molecules/ProtectedRoute';
+import Navbar from './components/molecules/Navbar';
 
 // Pages import
-import AuthPage from './components/pages/AuthPage';
 import HomePage from './components/pages/HomePage';
 import AppointmentsPage from './components/pages/AppointmentsPage';
 import PeoplePage from './components/pages/PeoplePage';
@@ -17,65 +17,66 @@ import LogoutPage from './components/pages/LogoutPage';
 import AddAppointment from './components/organisms/AddAppointment';
 import UserAuthPage from './components/pages/UserAuthPage';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3004';
-
 export default function App() {
-  // state to check jwt from backend
-  const [auth, setAuth] = useState(true);
-
-  // on load, check for token... if no token,
-  // useEffect(() => {
-  //   const token = localStorage.getItem('authToken');
-  //   if (!token) return setAuth(false)/
-
-  //   ( async() => {
-  //     try {
-  //       const headers = {
-  //         headers: { Authorization: `Bearer ${token}` }
-  //       };
-
-  //       const response = await axios.get(`${BACKEND_URL}/authenticate`, headers);
-  //       const { valid } = response.data;
-  //       if (!valid) return setAuth(false);
-  //       setAuth(true);
-  //     } catch (err) {
-  //       setAuth(false);
-  //       console.error(err.response);
-  //     }
-  //   })();
-  // });
-
   return (
     <MedicalProvider>
       <Router>
-        { auth ? (
-          <div className="main-container w-screen h-screen
+        <div className="main-container w-screen h-screen
           flex flex-col items-center"
-          >
-            <div className="nav-div w-full border-solid border-black border-2">
-              <nav className="flex justify-evenly items-center">
-                <NavLink className={({ isActive }) => `${isActive && 'font-bold'} hover:underline`} to="/">Home</NavLink>
-                <NavLink className={({ isActive }) => `${isActive && 'font-bold'} hover:underline`} to="/appointments">Appointments</NavLink>
-                <NavLink className={({ isActive }) => `${isActive && 'font-bold'} hover:underline`} to="/people">People</NavLink>
-                <NavLink className={({ isActive }) => `${isActive && 'font-bold'} hover:underline`} to="/profile">Profile</NavLink>
-                <NavLink className={({ isActive }) => `${isActive && 'font-bold'} hover:underline`} to="/logout">Logout</NavLink>
-              </nav>
-            </div>
-            <div className="display">
-              <Routes>
-                <Route path="/" element={<UserAuthPage />} />
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/appointments" element={<AppointmentsPage />} />
-                <Route path="/people" element={<PeoplePage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/logout" element={<LogoutPage setAuth={setAuth} />} />
-                <Route path="/add-appt" element={<AddAppointment />} />
-              </Routes>
-            </div>
+        >
+
+          <div className="display">
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={<UserAuthPage />}
+              />
+              <Route
+                exact
+                path="/nav/"
+                element={<Navbar />}
+              >
+                <Route
+                  index
+                  element={<ProtectedRoute><HomePage /></ProtectedRoute>}
+                />
+                <Route
+                  exact
+                  path="home"
+                  element={<ProtectedRoute><HomePage /></ProtectedRoute>}
+                />
+                <Route
+                  exact
+                  path="appointments"
+                  element={<ProtectedRoute><AppointmentsPage /></ProtectedRoute>}
+                />
+                <Route
+                  exact
+                  path="people"
+                  element={<ProtectedRoute><PeoplePage /></ProtectedRoute>}
+                />
+                <Route
+                  exact
+                  path="profile"
+                  element={<ProtectedRoute><ProfilePage /></ProtectedRoute>}
+                />
+                <Route
+                  exact
+                  path="add-appt"
+                  element={<ProtectedRoute><AddAppointment /></ProtectedRoute>}
+                />
+
+              </Route>
+              <Route
+                exact
+                path="/logout"
+                element={<LogoutPage />}
+              />
+
+            </Routes>
           </div>
-        )
-          // Log In page displays if there's no auth token
-          : <AuthPage /> }
+        </div>
       </Router>
     </MedicalProvider>
   );
