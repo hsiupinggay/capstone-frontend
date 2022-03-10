9b3e8361c8436054d009f6c90fba009cca968f25
Nuuggs <bryluke000@gmail.com>
2022-03-10 14:27:37 +0800

    cleared react errors

--- src/components/pages/AppointmentsPage.jsx
+++ src/components/pages/AppointmentsPage.jsx
@@ -1,35 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/solid";
-// import classNames from "./helper";
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/solid';
 
 // Global variables to help print  Calendar
 const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
 ];
 
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
-const AppointmentsPage = () => {
+function AppointmentsPage() {
   // Get today's date
-  const date = new Date();
+  const stateDate = new Date();
   // Set state to toggle between month views
-  const [month, setMonth] = useState(date.getMonth());
-  const [year, setYear] = useState(date.getFullYear());
+  const [month, setMonth] = useState(stateDate.getMonth());
+  const [year] = useState(stateDate.getFullYear());
   const [numOfDays, setNumOfDays] = useState([]);
   const [emptyDays, setEmptyDays] = useState([]);
 
@@ -42,20 +41,20 @@ const AppointmentsPage = () => {
     return today.toDateString() === d.toDateString();
   };
 
-  // Function to define number of days in the month and check the empty days at the beginning of the month
+  // Function: define daysInMonth & check emptyDays at start of month
   const getNoOfDays = () => {
     let i;
-    let daysInMonth = new Date(year, month + 1, 0).getDate();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
 
     // find where to start calendar day of week
-    let dayOfWeek = new Date(year, month).getDay();
-    let emptyDaysArray = [];
-    for (i = 1; i <= dayOfWeek; i++) {
+    const dayOfWeek = new Date(year, month).getDay();
+    const emptyDaysArray = [];
+    for (i = 1; i <= dayOfWeek; i += 1) {
       emptyDaysArray.push(i);
     }
 
-    let daysArray = [];
-    for (i = 1; i <= daysInMonth; i++) {
+    const daysArray = [];
+    for (i = 1; i <= daysInMonth; i += 1) {
       daysArray.push(i);
     }
 
@@ -72,57 +71,55 @@ const AppointmentsPage = () => {
   const events = [
     {
       event_date: new Date(2022, 8, 1),
-      event_title: "My Birthday :)",
-      event_theme: "red"
+      event_title: 'My Birthday :)',
+      event_theme: 'red',
     },
 
     {
       event_date: new Date(20212, 11, 25),
-      event_title: "Xmas Day",
-      event_theme: "green"
+      event_title: 'Xmas Day',
+      event_theme: 'green',
     },
     {
       event_date: new Date(2022, 9, 31),
-      event_title: "Halloween",
-      event_theme: "yellow"
+      event_title: 'Halloween',
+      event_theme: 'yellow',
     },
     {
       event_date: new Date(2021, 11, 31),
-      event_title: "New Years Eve",
-      event_theme: "yellow"
-    }
+      event_title: 'New Years Eve',
+      event_theme: 'yellow',
+    },
   ];
 
-  const themes = [
-    {
-      value: "blue",
-      label: "Blue Theme"
-    },
-    {
-      value: "red",
-      label: "Red Theme"
-    },
-    {
-      value: "yellow",
-      label: "Yellow Theme"
-    },
-    {
-      value: "green",
-      label: "Green Theme"
-    },
-    {
-      value: "purple",
-      label: "Purple Theme"
-    }
-  ];
+  // const themes = [
+  //   {
+  //     value: 'blue',
+  //     label: 'Blue Theme',
+  //   },
+  //   {
+  //     value: 'red',
+  //     label: 'Red Theme',
+  //   },
+  //   {
+  //     value: 'yellow',
+  //     label: 'Yellow Theme',
+  //   },
+  //   {
+  //     value: 'green',
+  //     label: 'Green Theme',
+  //   },
+  //   {
+  //     value: 'purple',
+  //     label: 'Purple Theme',
+  //   },
+  // ];
 
   // Arrow button classNames
-  const btnClass = (limit) => {
-    return classNames(
-      month === limit ? "cursor-not-allowed opacity-25" : "",
-      "leading-none rounded-lg transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-200 p-1 items-center focus:outline-none"
-    );
-  };
+  const btnClass = (limit) => classNames(
+    month === limit ? 'cursor-not-allowed opacity-25' : '',
+    'leading-none rounded-lg transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-200 p-1 items-center focus:outline-none',
+  );
 
   // Function for right arrow
   const nextMonth = () => {
@@ -137,16 +134,16 @@ const AppointmentsPage = () => {
 
   const eventClass = (t) => {
     switch (t) {
-      case "blue":
-        return "border-blue-200 text-blue-800 bg-blue-100";
-      case "red":
-        return "border-red-200 text-red-800 bg-red-100";
-      case "yellow":
-        return "border-yellow-200 text-yellow-800 bg-yellow-1000";
-      case "green":
-        return "border-green-200 text-green-800 bg-green-100";
+      case 'blue':
+        return 'border-blue-200 text-blue-800 bg-blue-100';
+      case 'red':
+        return 'border-red-200 text-red-800 bg-red-100';
+      case 'yellow':
+        return 'border-yellow-200 text-yellow-800 bg-yellow-1000';
+      case 'green':
+        return 'border-green-200 text-green-800 bg-green-100';
       default:
-        return "border-purple-200 text-purple-800 bg-purple-100";
+        return 'border-purple-200 text-purple-800 bg-purple-100';
     }
   };
 
@@ -156,7 +153,7 @@ const AppointmentsPage = () => {
       <div className="container mx-auto py-4 px-6">
         {/* calendar outer frame */}
         <div className="bg-white rounded-lg shadow overflow-hidden">
-          {/* top row: display month, year; display nav arrows*/}
+          {/* top row: display month, year; display nav arrows */}
           <div className="flex items-center justify-between px-6 py-4 border-b">
             {/* div: month and year */}
             <div>
@@ -166,22 +163,12 @@ const AppointmentsPage = () => {
             {/* div: nav arrow buttons */}
             <div className="border rounded-lg px-1 pt-1">
               {/* Previous Month Button */}
-              <button
-                type="button"
-                onClick={() => prevMonth()}
-                disabled={month === 0}
-                className={btnClass(0)}
-                >
+              <button type="button" onClick={() => prevMonth()} disabled={month === 0} className={btnClass(0)}>
                 <ArrowLeftIcon className="h-6 w-6 text-gray-500 inline-flex leading-none" />
               </button>
               <div className="border-r inline-flex h-6" />
               {/* Next Month Button */}
-              <button
-                type="button"
-                onClick={() => nextMonth()}
-                disabled={month === 11}
-                className={btnClass(11)}
-                >
+              <button type="button" onClick={() => nextMonth()} disabled={month === 11} className={btnClass(11)}>
                 <ArrowRightIcon className="h-6 w-6 text-gray-500 inline-flex leading-none" />
               </button>
             </div>
@@ -189,7 +176,7 @@ const AppointmentsPage = () => {
           {/* Calendar Body */}
           <div className="-mx-1 -mb-1">
             {/* Div: Print Day Row */}
-            <div className="flex flex-wrap -mb-8" style={{ marginBottom: "-30px" }}>
+            <div className="flex flex-wrap -mb-8" style={{ marginBottom: '-30px' }}>
               {days.map((day) => (
                 <div key={day} className="px-2 py-2 w-[14.28%]">
                   <div className="text-gray-600 text-sm uppercase tracking-wide font-bold text-center">{day}</div>
@@ -202,15 +189,10 @@ const AppointmentsPage = () => {
               {emptyDays.map((emptyDay) => (
                 <div key={emptyDay} className="border-r border-b px-4 pt-2 h-32 w-[14.28%]" />
               ))}
-              {/* Print {date} and conditionally format for isToday vs. other days + add hover to show mouseover - probably can add onClick here to open modal */}
-              {numOfDays.map((date, index) => (
-                <div key={index} className="px-4 pt-2 border-r border-b relative h-32 w-[14.28%]">
-                  <div className={classNames(
-                    isToday(date)
-                      ? "bg-blue-500 text-white"
-                      : "text-gray-700 hover:bg-blue-200",
-                        "inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100"
-                  )}>
+              {/* Print {date} and conditionally format - can add onClick here to open modal */}
+              {numOfDays.map((date) => (
+                <div key={date} className="px-4 pt-2 border-r border-b relative h-32 w-[14.28%]">
+                  <div className={classNames(isToday(date) ? 'bg-blue-500 text-white' : 'text-gray-700 hover:bg-blue-200', 'inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100')}>
                     {date}
                   </div>
                   {/* Within non-empty days, check for event and print event */}
